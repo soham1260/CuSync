94094ab62153070ea01f1385c77f0ca913a27343
Soham <1260soham@gmail.com>
2026-07-05 15:31:57 +0530

    Added helper function to create pipeline

--- src/NvDecPipeline.cpp
+++ src/NvDecPipeline.cpp
@@ -1,6 +1,7 @@
 #include "../include/NvDecPipeline.h"
+#include <cuda_runtime.h>
 
-NvDecPipeline::NvDecPipeline() : demuxer(nullptr), decoder(nullptr), d_bgrFrame(nullptr) {}
+NvDecPipeline::NvDecPipeline() : demuxer(nullptr), decoder(nullptr), d_bgrFrame(nullptr),width(0), height(0), finished(false) {}
 
 NvDecPipeline::~NvDecPipeline() 
 {
